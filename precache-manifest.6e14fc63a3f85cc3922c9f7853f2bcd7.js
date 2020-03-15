d5d78cd6e1b019386e11b7371439e85e1cac9e66
Vitor Jordao <srvitorlol@gmail.com>
2020-03-15 13:51:27 -0300

    Updates

---
+++ precache-manifest.6e14fc63a3f85cc3922c9f7853f2bcd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04bd5a05668ca7716f192ec6149de9f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6bcbe11c5f17b8db29c",
+    "url": "/static/css/main.aa360f04.chunk.css"
+  },
+  {
+    "revision": "d63d256d057a07eb52b3",
+    "url": "/static/js/2.6cbf04e4.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.6cbf04e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6bcbe11c5f17b8db29c",
+    "url": "/static/js/main.6aec62ef.chunk.js"
+  },
+  {
+    "revision": "39d6f10df612a0f522ac",
+    "url": "/static/js/runtime-main.e9aa8d15.js"
+  }
+]);
