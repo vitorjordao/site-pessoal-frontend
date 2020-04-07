0b743204de286d0d647b546204fd49d32486d6ce
Vitor Jordao <srvitorlol@gmail.com>
2020-04-07 16:44:29 -0300

    Fix Router for found routes on github pages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Header from './header/Header';
 import Main from './main/Main';
 import Footer from './footer/Footer';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header />
       <Main />
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
